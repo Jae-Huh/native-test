85537b6608345f0353c3ffe109e17ec45a54c349
Jae Huh <jaeeunhuh@gmail.com>
2017-06-10 17:52:05 +1200

    Add buttons and scrollability

--- App.js
+++ App.js
@@ -1,14 +1,28 @@
 import React from 'react'
-import { StyleSheet, Text, View, Image } from 'react-native'
+import { StyleSheet, Text, TextInput, View, Image, Button, TouchableOpacity, ScrollView } from 'react-native'
 
 export default class App extends React.Component {
   render() {
     const puppy = {uri: 'https://s-media-cache-ak0.pinimg.com/736x/3b/3b/35/3b3b359f28443b5651e97586dd0c0bf7.jpg'}
     return (
-      <View style={styles.container}>
-        <Text style={styles.titleText}>Puppy!</Text>
-        <Image style={styles.puppyPic} source={puppy} />
-      </View>
+      <ScrollView maximumZoomScale={2}>
+        <View style={styles.container}>
+          <TextInput placeholder="Text input field" style={styles.textInput}/>
+          <Text style={styles.titleText}>Puppy!</Text>
+          <Image style={styles.puppyPic} source={puppy} />
+          <Button title="Button" onPress={() => alert('Just a Button')} />
+          <TouchableOpacity onPress={() => alert('TouchableOpacity')}>
+            <View style={styles.button}>
+              <Text style={styles.buttonText}>TouchableOpacity</Text>
+            </View>
+          </TouchableOpacity>
+          <View style={styles.innerContainer}>
+            <View style={{width: 50, height: 50, backgroundColor: 'powderblue'}} />
+            <View style={{width: 50, height: 50, backgroundColor: 'skyblue'}} />
+            <View style={{width: 50, height: 50, backgroundColor: 'steelblue'}} />
+          </View>
+        </View>
+      </ScrollView>
     )
   }
 }
@@ -28,7 +42,26 @@ const styles = StyleSheet.create({
     padding: 10
   },
   puppyPic: {
-    width: 193,
-    height: 210
+    width: 80,
+    height: 80
+  },
+  textInput: {
+    height: 50,
+    borderWidth: 1,
+    margin: 20,
+    paddingHorizontal: 10
+  },
+  button: {
+    marginBottom: 20,
+    width: 200,
+    alignItems: 'center',
+    backgroundColor: '#2196F3'
+  },
+  buttonText: {
+    padding: 20,
+    color: 'white'
+  },
+  innerContainer: {
+    flexDirection: 'row'
   }
 });
